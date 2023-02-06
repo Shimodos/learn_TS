83856512d47c8b4a9df9a53d853e703e935befc3
Alekssey Demchenko <ad@pesochin.net>
2023-02-06 21:05:33 +0200

    special type any

--- test/index.ts
+++ test/index.ts
@@ -7,17 +7,29 @@
 // 10, 0.5, 0.00001, -50, 4e10 числа
 // true, false буливые значения
 
-const isBirthdayData: boolean = true;
-let ageData: number = 40;
-const userNameData: string = "Alex";
+// const isBirthdayData: boolean = true;
+// let ageData: number = 40;
+// const userNameData: string = "Alex";
 
-// function logBrtMsg(isBirthday: boolean, userName: string, age: number): string {
-//     if (isBirthday) {
-//         return `Congrats ${userName.toUpperCase()}, age: ${age + 1}`;
-//     } else {
-//         return "Sorry"
-//     }
-// }
+// // function logBrtMsg(isBirthday: boolean, userName: string, age: number): string {
+// //     if (isBirthday) {
+// //         return `Congrats ${userName.toUpperCase()}, age: ${age + 1}`;
+// //     } else {
+// //         return "Sorry"
+// //     }
+// // }
+
+let salary: number;
+salary = 500;
+
+const userData = '{"isBirthdayData": true, "ageData": 40, "userNameData": "Alex"}';
+
+const userObj: {
+	isBirthdayData: boolean;
+	userNameData: string;
+	ageData: number;
+} = JSON.parse(userData);
+console.log(userObj.smt);
 
 const logBrtMsg = (isBirthday: boolean, userName: string, age: number): string => {
 	if (isBirthday) {
