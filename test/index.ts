b7f111ec423c3a3a78c295ab889d693e5f8ed830
Alekssey Demchenko <ad@pesochin.net>
2024-03-04 18:14:59 +0200

    TS: 2.27: Механизм вывода типов (Type Inference)

--- test/index.ts
+++ test/index.ts
@@ -1,85 +1,22 @@
-// Primitive literal type
+let salary: number;
+salary = 1000;
 
-let msg: 'hello' = 'hello';
-
-msg = 'hello'; // OK
-
-// type Config = { protocol: 'http' | 'https'; port: 3000 | 3001 };
-interface IConfig {
-  protocol: 'http' | 'https';
-  port: 3000 | 3001;
-  log: (msg: string) => void;
-}
-// type Role = {
-//   role: string;
-// };
-// type ConfigWithRole = Config & Role;
-
-interface IRole {
-  role: string;
+interface UserData {
+  name: string;
+  age: number;
+  city: string;
 }
 
-interface IConfig {
-  data: Date;
+const userData = '{"name": "John", "age": 30, "city": "New York"}';
+
+const arr = ['123', 5, true];
+
+const user: UserData = JSON.parse(userData);
+console.log(user.name);
+
+let isOk = true;
+let movement: boolean | string = false;
+
+if (isOk) {
+  movement = 'forward';
 }
-
-interface IConfigWithRole extends IConfig, IRole {}
-
-const IConfigWithRole: IConfigWithRole = {
-  port: 3000,
-  protocol: 'https',
-  role: 'admin',
-  data: new Date(),
-
-  log: (msg: string): void => console.log(msg),
-};
-
-// const backupConfig: ConfigWithRole = {
-//   port: 3001,
-//   protocol: 'http',
-//   role: 'user',
-// };
-
-type StartFunction = (
-  protocol: 'http' | 'https',
-  port: 3000 | 3001,
-  log: (msg: string) => void,
-) => string;
-
-const startServer: StartFunction = (
-  protocol: 'http' | 'https',
-  port: 3000 | 3001,
-  log: (msg: string) => void,
-): 'Server started' => {
-  log(`Server started on ${protocol}://localhost:${port}`);
-  return 'Server started';
-};
-
-type AnimationTimingFunction = 'ease' | 'ease-out' | 'ease-in'; // Псевдоним типа
-type AnimationID = string | number;
-
-function createAnimation(
-  id: AnimationID,
-  animName: string,
-  timingFunc: AnimationTimingFunction = 'ease',
-  duration: number,
-  iterCount: 'infinite' | number,
-): void {
-  // const elem = document.getElementById(`#${id}`) as HTMLElement;
-  // if (elem) {
-  console.log(` ${animName} ${timingFunc} ${duration}s ${iterCount}`);
-  // elem.style.animation = `${animName} ${timingFunc} ${duration}s ${iterCount}`;
-  // }
-}
-
-createAnimation('id', 'fade', 'ease-in', 2, 'infinite'); // OK
-
-interface Styles {
-  [key: string]: string;
-}
-
-const styles: Styles = {
-  position: 'absolute',
-  top: '20px',
-  left: '20px',
-};
