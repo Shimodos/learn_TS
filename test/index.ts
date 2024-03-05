05feb0a49135244ec553b42e34b61d2f41bc951d
Alekssey Demchenko <ad@pesochin.net>
2024-03-05 18:54:09 +0200

    TS: 2.30: Модификаторы свойств: readonly (Property Modifiers)

--- test/index.ts
+++ test/index.ts
@@ -1,5 +1,5 @@
 interface User {
-  login: string;
+  readonly login: string;
   password: string;
   age: number;
   isDeleted?: boolean;
@@ -18,8 +18,27 @@ const user: User = {
   address: 'Kyiv',
 };
 
+const userFreeze: Readonly<User> = {
+  login: 'user',
+  password: 'qwerty',
+  age: 25,
+  address: 'Kyiv',
+};
+
+userFreeze.password = 'admin';
+
+user.password = 'admin';
+
 const dbName = 'users';
 
 function sentUserData(obj: User, db?: string): void {
   console.log(obj.parents?.father?.toLowerCase(), db?.toLowerCase());
 }
+
+const basicPorts: ReadonlyArray<number> = [80, 443, 8080];
+basicPorts[0] = 5;
+basicPorts.push(3000);
+
+// const basicPorts: readonly [number, ...string[]] = [80, '443', '8080'];
+// basicPorts[0] = 5;
+// basicPorts.push(3000);
