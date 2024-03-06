123927a6e7f7c71c560d029ef5623235bd5c1447
Alekssey Demchenko <ad@pesochin.net>
2024-03-06 18:57:22 +0200

    TS: 2.33: Type queries

--- test/index.ts
+++ test/index.ts
@@ -1,51 +1,18 @@
-// let smth: unknown;
-// smth = 'Hello, World!';
+const dataFormatControl = {
+  water: 200,
+  electricity: 100,
+};
 
-// let data: string[] = smth;
-// data.find((e) => e);
+function checkResulting(data: typeof dataFormatControl): boolean {
+  const dataFromUser = {
+    water: 200,
+    electricity: 350,
+  };
 
-// const someValue: unknown = 10
-// someValue.method()
-
-// function fetchData(data: unknown): void {
-//   if (typeof data === 'string') {
-//     console.log(data.toUpperCase());
-//   }
-// }
-
-const userData = '{"isBirthdayData": true ,"name": "John" , "age": 30}';
-
-function safePars(s: string): unknown {
-  return JSON.parse(s);
+  if (data.electricity === dataFromUser.electricity && data.water === dataFromUser.water) {
+    return true;
+  } else return false;
 }
 
-const data = safePars(userData);
-
-function transferData(d: unknown): void {
-  if (typeof d === 'string') {
-    console.log(d.toUpperCase());
-  } else if (typeof d === 'object' && d) {
-    console.log(data);
-  } else {
-    console.error('Wrong data type');
-  }
-}
-
-transferData(data);
-
-try {
-  if (1) {
-    throw new Error('Error');
-  }
-} catch (e) {
-  if (e instanceof Error) {
-    console.error(e.message);
-  } else if (typeof e === 'string') {
-    console.error(e);
-  }
-}
-
-type T0 = any | unknown; // any
-type T1 = number | unknown; // unknown Перекрывает другие типы
-type T2 = string & unknown;
-type T3 = unknown & any; // any
+const PI = 3.14;
+let PICloned: typeof PI;
