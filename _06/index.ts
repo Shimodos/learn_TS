9d6381eb686c09b8fe713e3e086ed0f5eeea8c4f
Alekssey Demchenko <ad@pesochin.net>
2024-03-06 19:19:55 +0200

    TS: 2.34: Practis

---
+++ _06/index.ts
@@ -0,0 +1,68 @@
+// Перечисление с названием TypesOfMedia, которое включает строчные типы video, audio
+
+// Перечисление с названием FormatsOfMedia, которое включает строчные видео-форматы: .mp4, .mov, .mkv, .flv, .webM
+
+// Описание интерфейса, в котором:
+// name - строка
+// type - один из перечисления выше
+// format = один из перечисления выше
+// subtitles - необязательное поле типа строка
+// marks - необязательное поле неизвестного типа
+
+enum TypesOfMedia {
+  Video = 'video',
+  Audio = 'audio',
+}
+
+enum FormatsOfMedia {
+  MP4 = '.mp4',
+  MOV = '.mov',
+  MKV = '.mkv',
+  FLV = '.flv',
+  WEBM = '.webM',
+}
+
+interface MediaProps {
+  name: string;
+  type: TypesOfMedia;
+  format: FormatsOfMedia;
+  subtitles?: string;
+  marks?: unknown;
+}
+
+function playMedia(
+  { name, type, format, subtitles, marks }: MediaProps = {
+    name: 'example',
+    type: TypesOfMedia.Video,
+    format: FormatsOfMedia.MP4,
+  },
+): string {
+  let marksLog: string;
+  if (Array.isArray(marks)) {
+    marksLog = marks.join(' ');
+  } else if (typeof marks === 'string') {
+    marksLog = marks;
+  } else {
+    marksLog = 'Unsupported type of marks';
+  }
+
+  // Создать функционал, что если marks - это массив, то "сложить" все эелементы в одну строку и поместить в marksLog
+  // Если это строка, то просто поместить её в marksLog
+  // Если что-то другое - то marksLog = "Unsupported type of marks"
+  // Не допускайте any!
+
+  console.log(`Media ${name}${format} is ${type}
+    Marks: ${marksLog}
+    Subtitles: ${subtitles ?? 'none'}`);
+  // помните что это за оператор ??
+
+  return 'Media started';
+}
+
+playMedia({
+  name: 'WoW',
+  format: FormatsOfMedia.MOV,
+  type: TypesOfMedia.Video,
+  subtitles: 'hmhmhm hmhmhm doh',
+  marks: ['4:30', '5:40'],
+});
