5faee2be86c304769f1bd72a6ca923086b948d04
Alekssey Demchenko <ad@pesochin.net>
2024-03-07 19:06:54 +0200

    TS: 2.36: A little about internal types and type casting

--- test/index.ts
+++ test/index.ts
@@ -1,36 +1,35 @@
-const fetchData = (url: string, method: 'GET' | 'POST'): void => {
-  console.log(method);
+// let num: Number = new Number(5);
+// let num2: number = 5;
+// let num3 = Number(5);
+
+// num = num2;
+// num2 = num;
+
+const num = 5;
+const strNum: string = num.toString();
+const str = '5';
+const numStr: number = +str;
+
+interface Department {
+  name: string;
+  budget: number;
+}
+
+const department: Department = {
+  name: 'Development',
+  budget: 100000,
 };
 
-const reqOptions = {
-  url: 'https://api.com',
-  // method: 'POST' as const, // its type is 'POST' not string
-  method: 'POST', // its type is 'POST' not string
-};
+interface Project {
+  name: string;
+  projectBudget: number;
+}
 
-// fetchData('qqq', 'POST');
-// fetchData(reqOptions.url, reqOptions.method);
-fetchData(reqOptions.url, <'GET'>reqOptions.method);
+function transforrmDepartment(department: Department, amount: number): Project {
+  return {
+    name: department.name,
+    projectBudget: amount,
+  };
+}
 
-const box = document.querySelector('.box') as HTMLDivElement;
-const input = <HTMLInputElement>document.querySelector('input');
-
-const someNumber: number = +input.value;
-console.log(someNumber * 2);
-
-let a = 'value' as const;
-let b = {
-  f: 100,
-} as const;
-let c = [1, 2, 3] as const;
-
-// error
-let value = 'value';
-let arr = [1, 2, 3];
-let obj = {
-  f: 100,
-};
-
-// let T0 = value as const
-
-let T5 = (Math.round(Math.random() * 1) ? 'value' : 'value2') as const;
+const mainProject: Project = transforrmDepartment(department, 100000);
