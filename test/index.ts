a15e36878ee983809fe8c57e37a6d1af918cdccb
Alekssey Demchenko <ad@pesochin.net>
2024-03-11 19:35:34 +0200

    TS: 2.44: Using void inside TS

--- test/index.ts
+++ test/index.ts
@@ -1,56 +1,20 @@
-const forms = document.querySelectorAll('form');
-const email = document.querySelector('#email') as HTMLInputElement;
-const title = document.querySelector('#title') as HTMLInputElement;
-const text = document.querySelector('#text') as HTMLTextAreaElement;
-const checkbox = document.querySelector('#checkbox') as HTMLInputElement;
+type voidFunc = () => void;
 
-interface IFormData {
-  email: string;
-  title: string;
-  text: string;
-  checkbox: boolean;
-}
-
-const formData: IFormData = {
-  email: '',
-  title: '',
-  text: '',
-  checkbox: false,
+const retString: voidFunc = () => {
+  return 'hello';
 };
 
-forms.forEach((form) =>
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();
+const s = retString();
+console.log(s);
 
-    // Можно и создавать другой объект для соблюдения иммутабельности, но пока не обязательно
-    formData.email = email?.value ?? '';
-    formData.title = title?.value ?? '';
-    formData.text = text?.value ?? '';
-    formData.checkbox = checkbox?.checked ?? false;
+const retNum: voidFunc = () => {
+  return 1;
+};
 
-    if (validateFormData(formData)) {
-      checkFormData(formData);
-    }
-  }),
-);
+const n = retNum();
+console.log(n);
 
-function validateFormData(data: IFormData): boolean {
-  // Если каждое из свойств объекта правдиво...
-  if (Object.values(data).every((value) => value)) {
-    return true;
-  } else {
-    console.log('Please, complete all fields');
-    return false;
-  }
-}
+const names = ['Alice', 'Bob', 'Eve'];
+const newArr = names.slice();
 
-function checkFormData(data: IFormData) {
-  const { email } = data;
-  const emails = ['example@gmail.com', 'example@ex.com', 'admin@gmail.com'];
-
-  if (emails.some((e) => e === email)) {
-    console.log('This email is already exist');
-  } else {
-    console.log('Posting data...');
-  }
-}
+names.forEach((name, i, arr) => arr.push('newName'));
