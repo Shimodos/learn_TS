1a108212be1b5241de214962ecb1adacf4878cec
Alekssey Demchenko <ad@pesochin.net>
2024-03-19 19:30:14 +0200

    TS: 3.55: Indexed Access Types

--- test/index.ts
+++ test/index.ts
@@ -1,7 +1,24 @@
 interface ICompany {
   name: string;
   dabts: number;
+  departments: Department;
+  managment: {
+    owner: string;
+  };
 }
+
+interface Department {
+  [key: string]: string;
+}
+
+let debts = 'dabts' as const;
+type CompanyDebtsType = ICompany[typeof debts]; // Indexed Access Types
+
+// type CompanyDebtsType = ICompany['dabts']; // Indexed Access Types
+type CompanyOwnerType = ICompany['managment']['owner']; // Indexed Access Types
+type CompanyDepartmentsType = ICompany['departments'][number]; // Indexed Access Types
+type CompanyDepartmentsTypes = ICompany['departments']; // Indexed Access Types
+type Test = ICompany[keyof ICompany]; // Union Types
 
 type CompanyKeys = keyof ICompany;
 const keys: CompanyKeys = 'dabts';
@@ -13,6 +30,13 @@ function printDebts<T, K extends keyof T, S extends keyof T>(company: T, name: K
 const hh: ICompany = {
   name: 'hh',
   dabts: 100,
+  departments: {
+    sales: 'Sales',
+    marketing: 'Marketing',
+  },
+  managment: {
+    owner: 'Larry Page',
+  },
 };
 
 printDebts(hh, 'name', 'dabts');
@@ -20,6 +44,13 @@ printDebts(hh, 'name', 'dabts');
 const google: ICompany = {
   name: 'Google',
   dabts: 100,
+  departments: {
+    sales: 'Sales',
+    marketing: 'Marketing',
+  },
+  managment: {
+    owner: 'Larry Page',
+  },
 };
 
 printDebts(google, 'name', 'dabts');
