51029fcd888ed8120f93080e13d918c08d5a33a4
Alekssey Demchenko <ad@pesochin.net>
2024-03-25 19:46:13 +0200

    TS: 3.60: Utility types: Pick, Omit, Extract, Exclude, Record

--- test/index.ts
+++ test/index.ts
@@ -5,11 +5,34 @@ type Currencies = {
   ukraine: 'UAH';
 };
 
+type CurrWithoutUSA = Omit<Currencies, 'usa'>; // Исключаем из типа Currencies ключ 'usa'
+type CurrWithoutUSAAndUkraine = Pick<Currencies, 'usa' | 'ukraine'>; // Фильтрация по свойствам
+type CountriesWithoutUSA = Exclude<keyof Currencies, 'usa'>; // Исключаем из ключей типа Currencies ключ 'usa'
+type FadeType = Exclude<MyAnimation, 'swipe'>; // Исключаем из типа MyAnimation значение 'swipe'
+type SwipeType = Extract<MyAnimation | Direction, 'swipe'>; // Извлекаем из типа MyAnimation значение 'swipe'
+
 type CreateCustomCurr<T> = {
-  [P in keyof T as `cutom${Capitalize<string & P>}`]: string;
+  [P in keyof T as `custom${Capitalize<string & P>}`]: string;
 };
 
+type PlayersNames = 'Messi' | 'Ronaldo';
 type CustomCurrencies = CreateCustomCurr<Currencies>;
+type GameDataCurr = Record<PlayersNames, CustomCurrencies>;
+
+const gameData: GameDataCurr = {
+  Messi: {
+    customUsa: 'USD',
+    customEuro: 'EUR',
+    customYen: 'JPY',
+    customUkraine: 'UAH',
+  },
+  Ronaldo: {
+    customUsa: 'USD',
+    customEuro: 'EUR',
+    customYen: 'JPY',
+    customUkraine: 'UAH',
+  },
+};
 
 type MyAnimation = 'fade' | 'swipe';
 type Direction = 'In' | 'Out';
