18efd2704a0aec1006796b5f6af55c390a972940
Alekssey Demchenko <ad@pesochin.net>
2024-03-28 20:21:24 +0200

    TS: 3.63: Additional Utility types

--- test/index.ts
+++ test/index.ts
@@ -1,40 +1,20 @@
-type Currencies = {
-  usa: 'USD';
-  euro: 'EUR';
-  yen: 'JPY';
-  ukraine: 'UAH';
-};
+function calculate(a: number, b: number): number {
+  return a + b;
+}
 
-type CurrWithoutUSA = Omit<Currencies, 'usa'>; // Исключаем из типа Currencies ключ 'usa'
-type CurrWithoutUSAAndUkraine = Pick<Currencies, 'usa' | 'ukraine'>; // Фильтрация по свойствам
-type CountriesWithoutUSA = Exclude<keyof Currencies, 'usa'>; // Исключаем из ключей типа Currencies ключ 'usa'
-type FadeType = Exclude<MyAnimation, 'swipe'>; // Исключаем из типа MyAnimation значение 'swipe'
-type SwipeType = Extract<MyAnimation | Direction, 'swipe'>; // Извлекаем из типа MyAnimation значение 'swipe'
+type CalculateRT = ReturnType<typeof calculate>;
 
-type CreateCustomCurr<T> = {
-  [P in keyof T as `custom${Capitalize<string & P>}`]: string;
-};
+let anotheRes: CalculateRT = 10;
 
-type PlayersNames = 'Messi' | 'Ronaldo';
-type CustomCurrencies = CreateCustomCurr<Currencies>;
-type GameDataCurr = Record<PlayersNames, CustomCurrencies>;
+type CalculateParams = Parameters<typeof calculate>;
+type PT1 = Parameters<(a: number) => number>;
+type PT2 = Parameters<<T>(arg: T) => number>;
 
-const gameData: GameDataCurr = {
-  Messi: {
-    customUsa: 'USD',
-    customEuro: 'EUR',
-    customYen: 'JPY',
-    customUkraine: 'UAH',
-  },
-  Ronaldo: {
-    customUsa: 'USD',
-    customEuro: 'EUR',
-    customYen: 'JPY',
-    customUkraine: 'UAH',
-  },
-};
+class Example {
+  constructor(
+    public a: number,
+    public b: number,
+  ) {}
+}
 
-type MyAnimation = 'fade' | 'swipe';
-type Direction = 'In' | 'Out';
-
-type MyNewAnimation = `${MyAnimation}${Capitalize<Direction>}`;
+type T0 = ConstructorParameters<typeof Example>;
