b8e84dd756fd3c903482ff8a93a1509308202a9d
Alekssey Demchenko <ad@pesochin.net>
2024-04-02 19:03:12 +0300

    TS: 4.67: Basic class work

--- test/index.js
+++ test/index.js
@@ -1,31 +1,11 @@
-var jsonTest = '{"name": "John", "age": 30, "city": "New York"}';
-var obj = JSON.parse(jsonTest);
-var toDoList = [];
-// fetch('https://jsonplaceholder.typicode.com/todos/1')
-//   .then((response) => response.json())
-//   .then((json) => {
-//     if ('id' in json) {
-//       toDoList.push(json);
-//     }
-//     console.log(toDoList);
-//   });
-fetch('https://jsonplaceholder.typicode.com/todos')
-    .then(function (response) { return response.json(); })
-    .then(function (json) {
-    if ('id' in json && 'userId' in json && 'title' in json && 'completed' in json) {
-        toDoList.push(json);
+"use strict";
+class Box {
+    width;
+    height;
+    constructor(width) {
+        this.width = width;
+        this.height = 500;
     }
-    else if (Array.isArray(json)) {
-        toDoList = json;
-    }
-    else {
-        console.log("Error: ".concat(json));
-    }
-    console.log(toDoList);
-});
-var promise = new Promise(function (resolve, reject) {
-    reject('This is an error');
-});
-promise.then(function (value) {
-    console.log(value.toLowerCase());
-});
+}
+const firstBox = new Box(100);
+console.log(firstBox);
--- test/index.ts
+++ test/index.ts
@@ -1,18 +1,23 @@
-type FromPromise = Awaited<Promise<number>>; // string
+class Box {
+  width: number;
+  height: number;
 
-interface User {
+  constructor(width: number) {
+    this.width = width;
+    this.height = 500;
+  }
+}
+
+const firstBox = new Box(100);
+
+console.log(firstBox);
+
+class User {
   name: string;
   age: number;
 }
 
-async function getUser(): Promise<User[]> {
-  const users: User[] = [{ name: 'John', age: 30 }];
-  return users;
-}
-
-const users = getUser(); // Promise<User[]>
-
-type FetchUsersReturnType = Awaited<ReturnType<typeof getUser>>; // User[]
-
-type UnwrappedPromise<T> = T extends Promise<infer Return> ? Return : T;
-type UnwrappedPromiseType = UnwrappedPromise<ReturnType<typeof getUser>>; // User[]
+const ivan = new User();
+ivan.name = 'Ivan';
+ivan.age = 25;
+console.log(ivan);
