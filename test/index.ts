efc18da3c3b191ee445dce93e0600c8d6cbfa1ac
Alekssey Demchenko <ad@pesochin.net>
2024-04-05 20:02:47 +0300

    TS: 5.74: Property Visibility Modifiers

--- test/index.ts
+++ test/index.ts
@@ -1,100 +1,45 @@
-class Box {
-  width: number;
-  height: number = 500;
-  volume: number | undefined;
-  _content: string | undefined;
+class Player {
+  private login: string;
+  private _password: string;
+  public server: string;
+  protected consent: boolean;
 
-  constructor(width: number, volume?: number, content?: string) {
-    this.width = width;
-    this.volume = volume;
-    this._content = content;
+  get password() {
+    return this._password;
   }
 
-  calcVolume(): void {
-    if (!this.volume) {
-      this.volume = this.width * this.height;
-      console.log(`Volume is ${this.volume}`);
-    } else {
-      console.log(`Volume is ${this.volume}`);
-    }
-  }
-
-  // Overloading
-  checkBoxSize(transport: number): string;
-  checkBoxSize(transport: number[]): string;
-  checkBoxSize(transport: number | number[]): string {
-    if (typeof transport === 'number') {
-      return transport >= this.width ? 'Ok' : 'Not Ok';
-    } else {
-      return transport.some((item) => item >= this.width) ? 'Ok' : 'Not Ok';
-    }
-  }
-
-  // get content() {
-  //   return this._content;
-  // }
-
-  // set content(value) {
-  //   this._content = `Data: ${new Date().toLocaleDateString()} - ${value}`;
-  // }
-
-  async getContent(value: string) {
-    const data = await new Date().toLocaleDateString();
-    this._content = `Data: ${data} - ${value}`;
-    console.log(this._content);
-    // return this._content;
+  set password(newPassword: string) {
+    // Validation
+    this._password = newPassword;
   }
 }
 
-const firstBox = new Box(100);
-firstBox.volume = 1000;
-// console.log((firstBox.content = 'Books'));
-// console.log(firstBox.content);
+class CompetitivePlayer extends Player {
+  rank: number;
 
-class PresentBox extends Box {
-  wrap: string;
-  height: number = 300;
-
-  constructor(wrap: string, width: number) {
-    super(width);
-    this.wrap = wrap;
-  }
-
-  override async getContent(value: string, text?: string) {
-    const data = await new Date().toLocaleDateString();
-
-    if (!text) {
-      super.getContent(value);
-    } else {
-      text ? text : 'No text';
-    }
-
-    this._content = `Data: ${data} - ${value}, Text: ${text ? text : 'No text'}`;
-    console.log(this._content);
-    // return this._content;
+  isConsented() {
+    this.consent ? console.log('Consented') : console.log('Not consented');
   }
 }
 
-new PresentBox('Red', 200).getContent('Books', 'Hello');
+const player = new CompetitivePlayer();
+player.password = 'qsz1';
+player.server = 'EU';
+player.rank = 1;
 
 // class User {
-//   name: string;
-//   age: number;
+//   public email: string;
+//   public name: string;
+
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//   }
 // }
 
-// const ivan = new User();
-// ivan.name = 'Ivan';
-// ivan.age = 25;
-// console.log(ivan);
-
-class Styles {
-  [s: string]: string | ((s: string) => boolean);
-
-  // method() {
-  //   console.log('Method');
-  // }
-}
-
-const style = new Styles();
-style.color = 'red';
-style.fontSize = '16px';
+// class User {
+//   constructor(
+//     public email: string,
+//     public name: string,
+//   ) {}
+// }
