ac267d86448fd18f74aab67f73616d649749619a
Alekssey Demchenko <ad@pesochin.net>
2024-04-08 20:14:46 +0300

    TS: 5.77: this and context typing

--- test/index.ts
+++ test/index.ts
@@ -10,6 +10,10 @@ class Player {
   public server: string;
   protected consent: boolean;
 
+  constructor(login: string = 'player') {
+    this.#login = login;
+  }
+
   static {
     Player.game = setName();
   }
@@ -26,20 +30,48 @@ class Player {
   static getGame() {
     return Player.game;
   }
+
+  logIn = () => {
+    return `Player ${this.#login} logged in`;
+  };
+
+  connect() {
+    // Do smth
+    return this;
+  }
+
+  isPro(): this is CompetitivePlayer {
+    return this instanceof CompetitivePlayer;
+  }
 }
 
-new Player();
-new Player();
-new Player();
+// const player = new Player('test');
+// console.log(player.connect().logIn());
 console.log(Player.getGame());
+
+// const test = player.logIn;
+// test();
 
 class CompetitivePlayer extends Player {
   rank: number;
+
+  checkLogin() {
+    return this.logIn();
+  }
 
   isConsented() {
     this.consent ? console.log('Consented') : console.log('Not consented');
   }
 }
+
+const player = new Player('test');
+console.log(player.connect().logIn());
+
+const player2 = new CompetitivePlayer('test2');
+console.log(player2.connect().logIn());
+
+const somePlayer: Player | CompetitivePlayer = new CompetitivePlayer('test3');
+somePlayer.isPro() ? console.log(somePlayer) : console.log(somePlayer);
 
 // const player = new CompetitivePlayer();
 // player.password = 'qsz1';
