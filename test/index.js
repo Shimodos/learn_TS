1ded3adc7ba836443b408c77a8e6ac34a1b6c215
Alekssey Demchenko <ad@pesochin.net>
2024-04-08 20:33:53 +0300

    TS: 5.78: Abstract classes

--- test/index.js
+++ test/index.js
@@ -1,39 +1,15 @@
 "use strict";
-class Player {
-    #login;
-    _password;
-    server;
-    consent;
-    get password() {
-        return this._password;
-    }
-    set password(newPassword) {
-        // Validation
-        this._password = newPassword;
+class AbstractVechicle {
+    model;
+    capacity;
+    stopEngine(time) {
+        this.startEngine(time);
+        return `Engine stopped at ${time}`;
     }
 }
-const test = new Player();
-class CompetitivePlayer extends Player {
-    rank;
-    isConsented() {
-        this.consent ? console.log('Consented') : console.log('Not consented');
-    }
+class Vechicle extends AbstractVechicle {
+    startEngine = (time) => {
+        return `Engine started at ${time}`;
+    };
 }
-const player = new CompetitivePlayer();
-player.password = 'qsz1';
-player.server = 'EU';
-player.rank = 1;
-// class User {
-//   public email: string;
-//   public name: string;
-//   constructor(email: string, name: string) {
-//     this.email = email;
-//     this.name = name;
-//   }
-// }
-// class User {
-//   constructor(
-//     public email: string,
-//     public name: string,
-//   ) {}
-// }
+new Vechicle().startEngine(new Date());
--- test/index.ts
+++ test/index.ts
@@ -1,96 +1,23 @@
-function setName() {
-  return 'Hello';
+interface IEngine {
+  model: string;
+  capacity: number;
+  startEngine: (time: Date) => string;
 }
 
-class Player {
-  private static game: string;
-
-  #login: string;
-  private _password: string;
-  public server: string;
-  protected consent: boolean;
-
-  constructor(login: string = 'player') {
-    this.#login = login;
+abstract class AbstractVechicle {
+  model: string;
+  capacity: number;
+  abstract startEngine: (time: Date) => string;
+  stopEngine(time: Date): string {
+    this.startEngine(time);
+    return `Engine stopped at ${time}`;
   }
+}
 
-  static {
-    Player.game = setName();
-  }
-
-  get password() {
-    return this._password;
-  }
-
-  set password(newPassword: string) {
-    // Validation
-    this._password = newPassword;
-  }
-
-  static getGame() {
-    return Player.game;
-  }
-
-  logIn = () => {
-    return `Player ${this.#login} logged in`;
+class Vechicle extends AbstractVechicle {
+  startEngine = (time: Date) => {
+    return `Engine started at ${time}`;
   };
-
-  connect() {
-    // Do smth
-    return this;
-  }
-
-  isPro(): this is CompetitivePlayer {
-    return this instanceof CompetitivePlayer;
-  }
 }
 
-// const player = new Player('test');
-// console.log(player.connect().logIn());
-console.log(Player.getGame());
-
-// const test = player.logIn;
-// test();
-
-class CompetitivePlayer extends Player {
-  rank: number;
-
-  checkLogin() {
-    return this.logIn();
-  }
-
-  isConsented() {
-    this.consent ? console.log('Consented') : console.log('Not consented');
-  }
-}
-
-const player = new Player('test');
-console.log(player.connect().logIn());
-
-const player2 = new CompetitivePlayer('test2');
-console.log(player2.connect().logIn());
-
-const somePlayer: Player | CompetitivePlayer = new CompetitivePlayer('test3');
-somePlayer.isPro() ? console.log(somePlayer) : console.log(somePlayer);
-
-// const player = new CompetitivePlayer();
-// player.password = 'qsz1';
-// player.server = 'EU';
-// player.rank = 1;
-
-// class User {
-//   public email: string;
-//   public name: string;
-
-//   constructor(email: string, name: string) {
-//     this.email = email;
-//     this.name = name;
-//   }
-// }
-
-// class User {
-//   constructor(
-//     public email: string,
-//     public name: string,
-//   ) {}
-// }
+new Vechicle().startEngine(new Date());
