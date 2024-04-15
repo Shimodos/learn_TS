507f63bb1b2d10ca3e7efefe69b4301cbf4b290b
Alekssey Demchenko <ad@pesochin.net>
2024-04-15 18:55:01 +0300

    TS: 6.91: Decoradores ES para propiedades de acceso (5+)

--- test/index.ts
+++ test/index.ts
@@ -12,10 +12,12 @@ class myCar implements ICar {
   errors: any;
   _waight: number = 1000;
 
+  @logOnSet
   set waight(value: number) {
     this._waight = this._waight + value;
   }
-  @log
+
+  @logOnGet
   get waight() {
     return this._waight;
   }
@@ -29,73 +31,73 @@ class myCar implements ICar {
   }
 }
 
-function log(
-  target: Object,
-  propertyKey: string | symbol,
-  descriptor: PropertyDescriptor,
-): PropertyDescriptor | void {
-  const oldValue = descriptor.set;
-  const oldGet = descriptor.get;
-  descriptor.set = function (this: any, ...args: any) {
+function logOnSet<T, R>(
+  target: (this: T, value: number) => R,
+  context: ClassSetterDecoratorContext<T, number>,
+) {
+  return function (this: T, ...args: any): R {
     console.log(`Calling ${[...args]} with`);
-    return oldValue?.apply(this, args);
-  };
-  descriptor.get = function () {
-    console.log(`Test`);
-    return oldGet?.apply(this);
+    return target.apply(this, args);
   };
 }
 
-function checkNumberOfSeats(limit: number) {
-  return function (target: Object, propertyKey: string | symbol) {
-    let symbol = Symbol();
-
-    const getter = function (this: any) {
-      return this[symbol];
-    };
-
-    const setter = function (this: any, newVal: number) {
-      if (newVal >= 1 && newVal < limit) {
-        this[symbol] = newVal + 1;
-        // value = `You have ${newVal} seats left`;
-      } else {
-        // console.log(`To many seats ${limit}`);
-        Object.defineProperty(target, 'errors', {
-          value: `To many seats ${limit}`,
-        });
-      }
-    };
-    Object.defineProperty(target, propertyKey, {
-      get: getter,
-      set: setter,
-    });
+function logOnGet<T, R>(target: (this: T) => R, context: ClassGetterDecoratorContext<T, number>) {
+  return function (this: T): R {
+    console.log(`Test`);
+    return target.apply(this);
   };
 }
 
 // function checkNumberOfSeats(limit: number) {
-//   return function (target: undefined, context: ClassFieldDecoratorContext) {
-//     return function (this: any, newVal: number) {
+//   return function (target: Object, propertyKey: string | symbol) {
+//     let symbol = Symbol();
+
+//     const getter = function (this: any) {
+//       return this[symbol];
+//     };
+
+//     const setter = function (this: any, newVal: number) {
 //       if (newVal >= 1 && newVal < limit) {
-//         return newVal;
+//         this[symbol] = newVal + 1;
+//         // value = `You have ${newVal} seats left`;
 //       } else {
-//         throw Error('Not enough seats');
+//         // console.log(`To many seats ${limit}`);
+//         Object.defineProperty(target, 'errors', {
+//           value: `To many seats ${limit}`,
+//         });
 //       }
 //     };
+//     Object.defineProperty(target, propertyKey, {
+//       get: getter,
+//       set: setter,
+//     });
 //   };
 // }
 
-function checkAmountOfFuel(
-  target: Object,
-  propertyKey: string | symbol,
-  descriptor: PropertyDescriptor,
-): PropertyDescriptor | void {
-  const oldValue = descriptor.value;
-  descriptor.value = function (this: any, ...args: any[]) {
-    console.log(this.fule);
-
-    return oldValue.apply(this, args);
+function checkNumberOfSeats(limit: number) {
+  return function (target: undefined, context: ClassFieldDecoratorContext) {
+    return function (this: any, newVal: number) {
+      if (newVal >= 1 && newVal < limit) {
+        return newVal;
+      } else {
+        throw Error('Not enough seats');
+      }
+    };
   };
 }
+
+// function checkAmountOfFuel(
+//   target: Object,
+//   propertyKey: string | symbol,
+//   descriptor: PropertyDescriptor,
+// ): PropertyDescriptor | void {
+//   const oldValue = descriptor.value;
+//   descriptor.value = function (this: any, ...args: any[]) {
+//     console.log(this.fule);
+
+//     return oldValue.apply(this, args);
+//   };
+// }
 
 // function checkAmountOfFuel(target: any, context: ClassMethodDecoratorContext) {
 //   return function (this: any, ...args: any[]) {
@@ -104,42 +106,22 @@ function checkAmountOfFuel(
 //   };
 // }
 
-// function checkAmountOfFuel<T, A extends any[], R>(
-//   target: (this: T, ...args: A) => R,
-//   context: ClassMethodDecoratorContext<T, (this: T, ...args: A) => R>,
-// ) {
-//   return function (this: T, ...args: A): R {
-//     // console.log(this.fule);
-//     console.log(`${String(context.name)} is sttarted`);
-//     return target.apply(this, args);
-//   };
-// }
-
-function changeDoorStatus(status: boolean) {
-  console.log('door init');
-  return <T extends { new (...args: any[]): {} }>(constructor: T) => {
-    console.log('door changed');
-    return class extends constructor {
-      open = status;
-    };
-  };
-}
-
-function changeAmountOfFuel(amount: number) {
-  console.log('fuel init');
-  return <T extends { new (...args: any[]): {} }>(constructor: T) => {
-    console.log('fuil changed');
-    return class extends constructor {
-      fule = `${amount}%`;
-    };
+function checkAmountOfFuel<T, A extends any[], R>(
+  target: (this: T, ...args: A) => R,
+  context: ClassMethodDecoratorContext<T, (this: T, ...args: A) => R>,
+) {
+  return function (this: T, ...args: A): R {
+    // console.log(this.fule);
+    console.log(`${String(context.name)} is sttarted`);
+    return target.apply(this, args);
   };
 }
 
 // function changeDoorStatus(status: boolean) {
 //   console.log('door init');
-//   return <T extends { new (...args: any[]): {} }>(target: T, context: ClassDecoratorContext<T>) => {
+//   return <T extends { new (...args: any[]): {} }>(constructor: T) => {
 //     console.log('door changed');
-//     return class extends target {
+//     return class extends constructor {
 //       open = status;
 //     };
 //   };
@@ -147,13 +129,33 @@ function changeAmountOfFuel(amount: number) {
 
 // function changeAmountOfFuel(amount: number) {
 //   console.log('fuel init');
-//   return <T extends { new (...args: any[]): {} }>(target: T, context: ClassDecoratorContext<T>) => {
+//   return <T extends { new (...args: any[]): {} }>(constructor: T) => {
 //     console.log('fuil changed');
-//     return class extends target {
+//     return class extends constructor {
 //       fule = `${amount}%`;
 //     };
 //   };
 // }
+
+function changeDoorStatus(status: boolean) {
+  console.log('door init');
+  return <T extends { new (...args: any[]): {} }>(target: T, context: ClassDecoratorContext<T>) => {
+    console.log('door changed');
+    return class extends target {
+      open = status;
+    };
+  };
+}
+
+function changeAmountOfFuel(amount: number) {
+  console.log('fuel init');
+  return <T extends { new (...args: any[]): {} }>(target: T, context: ClassDecoratorContext<T>) => {
+    console.log('fuil changed');
+    return class extends target {
+      fule = `${amount}%`;
+    };
+  };
+}
 
 // function closeCare<T extends { new (...args: any[]): {} }>(constructor: T) {
 //   return class extends constructor {
